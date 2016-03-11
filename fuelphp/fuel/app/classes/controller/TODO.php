73b3f5f24903675fdabfdecc4e72085881b0d8f7
yuya-m-ms <maemichi@monosense.co.jp>
2016-03-11 17:10:05 +0900

    Add validation for new task

--- fuelphp/fuel/app/classes/controller/TODO.php
+++ fuelphp/fuel/app/classes/controller/TODO.php
@@ -13,4 +13,19 @@ class Controller_TODO extends Controller
         $view = View::forge('TODO', $data);
         return $view;
     }
+
+    /**
+     * @return Vadidation for a new task
+     */
+    public function forge_validation()
+    {
+        $val = Validation::forge();
+
+        $val->add('name', "Task name")
+            ->add_rule('trim')
+            ->add_rule('required')
+            ->add_rule('max_length', 100);
+
+        return $val;
+    }
 }
