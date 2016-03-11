718deb25c48c1365cdb17142a24af00a5d483f22
yuya-m-ms <maemichi@monosense.co.jp>
2016-03-11 18:52:13 +0900

    Replace todo list by todo table

--- fuelphp/fuel/app/classes/model/TODO.php
+++ fuelphp/fuel/app/classes/model/TODO.php
@@ -5,6 +5,6 @@
 */
 class Model_TODO extends Orm\Model
 {
-    protected static $_table_name = 'TODOs';
-    protected static $_properties = ['id', 'name', 'due', 'status', 'deleted'];
+    protected static $_table_name = 'TODO';
+    protected static $_properties = ['id', 'name', 'due', 'status_id', 'deleted'];
 }
