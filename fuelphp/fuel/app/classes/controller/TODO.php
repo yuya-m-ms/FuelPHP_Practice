61b7b7376e6c8faeb86edff81aa3046ca8ad7695
yuya-m-ms <maemichi@monosense.co.jp>
2016-03-14 18:59:22 +0900

    Implement 'change task' action

--- fuelphp/fuel/app/classes/controller/TODO.php
+++ fuelphp/fuel/app/classes/controller/TODO.php
@@ -77,6 +77,47 @@ class Controller_TODO extends Controller
         return Response::redirect('TODO');
     }
 
+    public function action_change($id)
+    {
+        if (Input::method() === 'POST') {
+            $val = $this->forge_validation();
+            if ($val->run()) {
+                $input = $val->validated();
+                $input['due_daytime'] = $input['due_day'] . ' ' . $input['due_time'];
+                // suppose no missing id
+                $todo = Model_TODO::find($id);
+                $todo->name = $input['name'];
+                // var_dump($input['due_daytime']);
+                if (! empty($input['due_daytime'])) {
+                    $todo->due = $input['due_daytime'];
+                } else {
+                    $todo->due = null;
+                }
+                $todo->save();
+            }
+        }
+
+        return Response::redirect('TODO');
+    }
+
+    // TODO: use POST?
+    public function action_to_change($id)
+    {
+        $todo = Model_TODO::find($id);
+        $data['task_to_be_changed']['id']   = $todo->id;
+        $data['task_to_be_changed']['name'] = $todo->name;
+        $data['task_to_be_changed']['due']  = $todo->due;
+        if (! empty($todo->due)) {
+            // $re_datetime = '/(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2})*/';
+            // list($_, $due_day, $due_time) = preg_match($re_datetime, $todo->due);
+            list($due_day, $due_time) = explode(' ', $todo->due);
+            $data['task_to_be_changed']['due_day']  = $due_day;
+            $data['task_to_be_changed']['due_time'] = $due_time;
+        }
+        $data['TODOs'] = Model_TODO::find('all');
+        return Response::forge(View::forge('TODO', $data));
+    }
+
     /**
      * @return Vadidation for a new task
      */
