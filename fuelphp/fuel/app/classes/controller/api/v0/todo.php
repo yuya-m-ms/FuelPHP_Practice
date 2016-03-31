76e334461ba0eba590b9f0521499a4c7b4112760
yuya-m-ms <maemichi@monosense.co.jp>
2016-03-31 16:36:43 +0900

    Create starting point

---
+++ fuelphp/fuel/app/classes/controller/api/v0/todo.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+* REST API
+*/
+class Controller_Api_V0_Todo extends Controller_Rest
+{
+    public function get_todo($id)
+    {
+        return;
+    }
+
+    public function get_todo_list()
+    {
+        return;
+    }
+
+    public function delete_todo($id)
+    {
+        return;
+    }
+
+    public function post_todo()
+    {
+        return;
+    }
+
+    public function put_todo($id)
+    {
+        return;
+    }
+
+    public function patch_todo($id)
+    {
+        return;
+    }
+}
