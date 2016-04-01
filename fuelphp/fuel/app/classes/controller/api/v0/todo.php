2f971a62d1e1ffd65496f2338e62e84a95834a7c
yuya-m-ms <maemichi@monosense.co.jp>
2016-04-01 11:25:00 +0900

    Implement function

--- fuelphp/fuel/app/classes/controller/api/v0/todo.php
+++ fuelphp/fuel/app/classes/controller/api/v0/todo.php
@@ -7,37 +7,64 @@ class Controller_Api_V0_Todo extends Controller_Rest
 {
     public function before()
     {
+        date_default_timezone_set('UTC');
         parent::before();
         Domain_Todo::before();
     }
 
     public function get_item($id)
     {
-        return;
+        return $this->response(Model_Todo::find($id));
     }
 
     public function get_list()
     {
-        return;
+        $todos = Domain_Todo::fetch_todo();
+        return $this->response($todos);
     }
 
     public function delete_item($id)
     {
-        return;
+        Domain_Todo::alter($id, ['deleted' => true]);
     }
 
     public function post_item()
     {
-        return;
+        $item = [
+            'name'      => Input::get('name'),
+            'due'       => Input::get('due'),
+            'status_id' => 0,
+            'deleted'   => false,
+            'user_id'   => Session::get('user_id'),
+        ];
+        Domain_Todo::add_todo($item);
     }
 
     public function put_item($id)
     {
-        return;
+        $item = [
+            'name'      => Input::get('name'),
+            'due'       => Input::get('due'),
+            'status_id' => Input::get('status_id'),
+            'deleted'   => Input::get('deleted'),
+            'user_id'   => Input::get('user_id'),
+        ];
+        Domain_Todo::alter($id, $item);
     }
 
     public function patch_item($id)
     {
-        return;
+        $item = [
+            'name'      => Input::get('name'),
+            'due'       => Input::get('due'),
+            'status_id' => Input::get('status_id'),
+            'deleted'   => Input::get('deleted'),
+            'user_id'   => Input::get('user_id'),
+        ];
+        $non_null = function ($value) {
+            return ! is_null($value);
+        };
+        array_filter($item, $non_null);
+        Domain_Todo::alter($id, $item);
     }
 }
